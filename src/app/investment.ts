3eeae4d5d6a7cb9d87d318bfd39ce5be0ec9bb41
Jason Nguyen <jason.nguyen@insperity.com>
2019-05-29 12:40:29 -0500

    Replaced the implementation of annualSalary with a simple public property

--- src/app/investment.ts
+++ src/app/investment.ts
@@ -11,34 +11,23 @@ export class NewInvestment implements Investment {
 }
 
 export class Summary {
+    annualSalary = 0;
     desiredPercentage = .15;
     investments: Investment[] = [
         {
             name: '401(k)',
             percentage: .06,
             amount: 8700
-          },
-          {
+        },
+        {
             name: '401(k) Roth',
             percentage: .04,
             amount: 87
-          }
+        }
     ];
 
-    private _annualSalary = 0;
-    private _desiredAmount = 0;
-
-    get annualSalary(): number {
-        return this._annualSalary;
-    }
-
-    set annualSalary(annualSalary: number) {
-        this._annualSalary = annualSalary;
-        this._desiredAmount = annualSalary * this.desiredPercentage;
-    }
-
     get desiredAmount(): number {
-        return this._desiredAmount;
+        return this.annualSalary * this.desiredPercentage;
     }
 
     get totalAmountInvested(): number {
@@ -46,6 +35,6 @@ export class Summary {
     }
 
     get totalPercentageInvested(): number {
-        return this.totalAmountInvested / this._annualSalary;
+        return this.totalAmountInvested / this.annualSalary;
     }
 }
